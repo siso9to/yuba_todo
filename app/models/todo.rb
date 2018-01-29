fd5b4818851b7991cf943e7a1dea8058ca877f26
siso9to <yonepin@gmail.com>
2018-01-30 00:32:20 +0900

    fix model spec

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,4 @@
 class Todo < ApplicationRecord
+  validates :description, presence: true, length: { maximum: 100 }
+  validates :detail, length: { maximum: 2000 }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,4 +4,12 @@ RSpec.describe Todo, type: :model do
   it "has a valid factory" do
     expect(build(:todo)).to be_valid
   end
+  
+  it "description is required" do
+    expect(build(:todo, description: '')).not_to be_valid
+    
+    todo = Todo.new(description: '')
+    todo.valid?
+    expect(todo.errors[:description]).to include("can't be blank")
+  end
 end
