a84b1ffb147afa47a58aaf34079c764284a2b1cd
siso9to <yonepin@gmail.com>
2018-01-29 17:51:16 +0900

    add spec to todo model

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe Todo, type: :model do
   it "has a valid factory" do
     expect(build(:todo)).to be_valid
   end
+  
+  it "description is required" do
+    todo = Todo.new(description: nil)
+    todo.valid?
+    expect(todo.errors[:description]).to include("can't be blank")
+  end
 end
